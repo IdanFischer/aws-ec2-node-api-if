3202bf8e0fced94c7b5b9c67781e85743ae472d1
Idan Fischer <Idanfischer5@gmail.com>
2023-02-23 15:38:04 -0500

    made aws api

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen('5002', () => console.log('listening on port 5002'))
+
+app.get('/', (req, res) => {
+  res.send("Api Working")
+})
